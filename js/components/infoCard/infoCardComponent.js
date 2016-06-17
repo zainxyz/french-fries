59851f21cbf598a7a7a06d3190de93bd3a90fd6c
Vinicius Santana <santanaviny@gmail.com>
2016-06-17 12:15:47 -0700

    infoCard in the house

---
+++ js/components/infoCard/infoCardComponent.js
@@ -0,0 +1,34 @@
+var infoCardComponent = React.createClass({
+  render: function() {
+    return (
+      <section>
+      	<header>
+      		<h1> Title</h1>
+      	</header>
+      	<main>
+      		<table>
+      			<tr>
+      				<td>Bla</td>
+      				<td>Ble</td>
+      			</tr>
+      		</table>
+      		<net>
+      			Net Goes here
+      		</net>
+      	</main>
+      	<footer>
+      		<p>Text</p>
+      	</footer>
+      </section>
+      )
+  }
+});
+
+// var PRODUCTS = [
+//   {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+//   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+//   {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+//   {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+//   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+//   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+// ];
