350c4a0717cb19b30ef740623ee1df57f06ccda8
Vinicius Santana <santanaviny@gmail.com>
2016-06-17 10:45:04 -0700

    Trying out new components

---
+++ js/components/info_card/infocardComponent.js
@@ -0,0 +1,20 @@
+var InfoCardComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+      	<header>Assets</header>
+      	<table>
+      		<tr>
+      			<td>B of A Checking & Savings</td>
+      			<td>$19,500.00</td>
+      		</tr>
+      		<tr>
+      			<td>B of A Checking & Savings</td>
+      			<td>$19,500.00</td>
+      		</tr>
+      	</table>
+      	<footer>What I own</footer>
+      </div>
+      )
+  }
+});
