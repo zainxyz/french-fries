d6ed3490c6dd724f9ac63b63e00902fef0a796a4
Zain Abidi <Zain@Administrators-MacBook-Pro.local>
2016-06-17 14:31:52 -0700

    updating the navBar component styles and markup

--- js/components/navBar/nav.js
+++ js/components/navBar/nav.js
@@ -1,41 +1,45 @@
 
 var NavButton = React.createClass({
-  buttonClick: function(){
-    console.log("this.props.name");
-    console.log(this.props.name.toLowerCase());
-    
-    var hash = this.props.name.toLowerCase();
+    buttonClick: function(){
+        console.log("this.props.name");
+        console.log(this.props.name.toLowerCase());
 
-    // Backbone.history.start();
-    Backbone.history.navigate(hash,true);
-  },
-  render: function(){
-    return (
-      <button className={this.props.name} onClick={this.buttonClick}>{this.props.name}</button>
-      );
-  }
+        var hash = this.props.name.toLowerCase();
+
+        // Backbone.history.start();
+        Backbone.history.navigate(hash,true);
+    },
+    render: function(){
+        return (
+            <li className={this.props.name.toLowerCase()}>
+                <a onClick={this.buttonClick}>{this.props.name}</a>
+            </li>
+        );
+    }
 });
 
 var NavBar = React.createClass({
-  render:function(){
-    var buttons = [];
-    this.props.buttons.forEach(function(key,value,obj){
-      buttons.push(<NavButton name={key.name} key={key.name}/>);
-
-  });
-    return (
-      <div>{buttons}</div>
-      );
-  }
+    render:function(){
+        var buttons = [];
+            this.props.buttons.forEach(function(key,value,obj){
+            buttons.push(<NavButton name={key.name} key={key.name}/>);
+        });
+        return (
+            <div className="navbar">
+                <a className="logo">Super Advisor</a>
+                <ul className="nav">{buttons}</ul>
+            </div>
+        );
+    }
 });
 
 var buttonArray = [
-  {name: 'Household'},
-  {name: 'Goals'},
-  {name: 'Accounts'},
+    {name: 'Household'},
+    {name: 'Goals'},
+    {name: 'Accounts'},
 ];
 
 ReactDOM.render(
-  <NavBar buttons={buttonArray}/>,
-  document.getElementById('app_nav')
+    <NavBar buttons={buttonArray}/>,
+    document.getElementById('app_nav')
 );
