e3c26e35bb07f3ac842655203b3aed59ab3901bc
Vinicius Santana <santanaviny@gmail.com>
2016-06-18 11:22:15 -0700

    Playing with a pure html financial snapshot

--- js/components/financialSnapshot/financialSnapshot.js
+++ js/components/financialSnapshot/financialSnapshot.js
@@ -1,17 +1,27 @@
 var FinancialSnapshotComponent = React.createClass({
 	render: function(){
 		return (
-			<section className="financial-snapshot">
-				<div>
-					<div>
-						Total Assets
-						$203,500.00
+			<section className = "financial-snapshot">
+				<div className = "assets-liabilities">
+					<div className = "assets">
+						<span>Total Assets</span>
+						<span>$203,500.00</span>
 					</div>
-					<div>
-
+					<div className = "liabilities">
+						<span>Total Liabilities</span>
+						<span>$203,500.00</span>
 					</div>
 				</div>
-				<div></div>
+				<div className = "cash-expenses">
+					<div className = "cash">
+						<span>Total Cash</span>
+						<span>$203,500.00</span>
+					</div>
+					<div className = "expenses">
+						<span>Total Expenses</span>
+						<span>$203,500.00</span>
+					</div>
+				</div>
 			</section>
 		);
 	}
