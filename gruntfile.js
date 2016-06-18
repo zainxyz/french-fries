40a28ad255a0b7e7c8d924262edb2897276f258d
Zain Abidi <Zain@Administrators-MacBook-Pro.local>
2016-06-17 19:53:44 -0700

    updating concat grunttask to watch for all .js files

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         // Concat all files into one file
         concat: {
             js: {
-                src: ['js/components/**/*.js', 'js/components/interface.js'],
+                src: ['js/**/*.js'],
                 dest: 'js/app.js'
             }
         },
